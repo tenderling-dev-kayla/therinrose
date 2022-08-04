3cf0fda3374e277c9f9612a4093baaa9bb2f8f76
tenderling-dev-kayla <110254344+tenderling-dev-kayla@users.noreply.github.com>
2022-08-04 15:07:00 -0500

    set header content

--- global-templates/navbar-rinrose-bootstrap5.php
+++ global-templates/navbar-rinrose-bootstrap5.php
@@ -8,64 +8,49 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
-?>
 
-<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
+?>
+<div class="collapse" id="navbarToggleExternalContent">
+ 	<div class="bg-dark p-4">
+    	<h5 class="text-white h4">Collapsed content</h5>
+    	<span class="text-muted">Toggleable via the navbar brand.</span>
+	</div>
+</div>
+<nav id="main-nav" class="navbar fiex-top navbar-expand-md navbar-light bg-white" aria-labelledby="main-nav-label">
 
 	<h2 id="main-nav-label" class="screen-reader-text">
 		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
 	</h2>
 
 
-	<div class="<?php echo esc_attr( $container ); ?>">
-
-		<!-- Your site title as branding in the menu -->
-		<?php if ( ! has_custom_logo() ) { ?>
-
-			<?php if ( is_front_page() && is_home() ) : ?>
-
-				<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
-			<?php else : ?>
-
-				<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-			<?php endif; ?>
-
-			<?php
-		} else {
-			the_custom_logo();
-		}
-		?>
-		<!-- end custom logo -->
+	<div class="container-fluid">
 
 		<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNavOffcanvas" aria-controls="navbarNavOffcanvas" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 			<span class="navbar-toggler-icon"></span>
 		</button>
 
-		<div class="offcanvas offcanvas-end bg-primary" tabindex="-1" id="navbarNavOffcanvas">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      		<span class="navbar-toggler-icon"></span>
+    	</button>
 
-			<div class="offcanvas-header justify-content-end">
-				<button type="button" class="btn-close btn-close-white text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-			</div><!-- .offcancas-header -->
+		<span class="navbar-text">
+        	<a href="#">Residents</a>
+      	</span>
 
-			<!-- The WordPress Menu goes here -->
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location'  => 'primary',
-					'container_class' => 'offcanvas-body',
-					'container_id'    => '',
-					'menu_class'      => 'navbar-nav justify-content-end flex-grow-1 pe-3',
-					'fallback_cb'     => '',
-					'menu_id'         => 'main-menu',
-					'depth'           => 2,
-					'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-				)
-			);
-			?>
-		</div><!-- .offcanvas -->
+		<!-- Your site title as branding in the menu -->
+		<?php if (get_field('logo', 'option')) : ?>
+			<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+				<?php echo wp_get_attachment_image( get_field('logo', 'option'), 'full', "", array( "class" => "img-responsive", "alt" => bloginfo( 'name' ) ) ); ?>
+			</a>
+		<?php endif; ?>
+		<!-- end custom logo -->
+
+		<span class="navbar-text">
+        	<a href="#">Tours</a>
+      	</span>
+      	<span class="navbar-text">
+        	<a href="#">Apply</a>
+      	</span>
 
 	</div><!-- .container(-fluid) -->
 
