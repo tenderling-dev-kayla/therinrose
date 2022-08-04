9589cb3fe2206a3fd471d687c168b079a58ec12e
tenderling-dev-kayla <110254344+tenderling-dev-kayla@users.noreply.github.com>
2022-08-04 15:55:01 -0500

    navbar update

--- global-templates/navbar-rinrose-bootstrap5.php
+++ global-templates/navbar-rinrose-bootstrap5.php
@@ -16,38 +16,36 @@ defined( 'ABSPATH' ) || exit;
     	<span class="text-muted">Toggleable via the navbar brand.</span>
 	</div>
 </div>
-<nav id="main-nav" class="navbar fiex-top navbar-light bg-white" aria-labelledby="main-nav-label">
-
-	<h2 id="main-nav-label" class="screen-reader-text">
-		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-	</h2>
-
-
+<nav id="main-nav" class="navbar fix-top navbar-light bg-white" aria-labelledby="main-nav-label">
+	<h2 id="main-nav-label" class="screen-reader-text"><?php esc_html_e( 'Main Navigation', 'tenderling' ); ?></h2>
 	<div class="container-fluid">
-
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-    		<span class="navbar-toggler-icon"></span>
-    	</button>
-
-		<span class="navbar-text">
-        	<a href="#">Residents</a>
-      	</span>
-
-		<!-- Your site title as branding in the menu -->
-		<?php if (get_field('logo', 'option')) : ?>
-			<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-				<?php echo wp_get_attachment_image( get_field('logo', 'option'), 'full', "", array( "class" => "img-responsive", "alt" => get_bloginfo( 'name' ) ) ); ?>
-			</a>
-		<?php endif; ?>
-		<!-- end custom logo -->
-
-		<span class="navbar-text">
-        	<a href="#">Tours</a>
-      	</span>
-      	<span class="navbar-text">
-        	<a href="#">Apply</a>
-      	</span>
-
+		<div class="row">
+			<div id="rinrose_header_nav-left" class="col-5">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+    				<span class="navbar-toggler-icon"></span>
+    			</button>
+				<span class="navbar-text">
+        			<a href="#">Residents</a>
+      			</span>
+      		</div>
+      		<div id="rinrose_header_nav-mind" class="col-2">
+				<!-- Your site title as branding in the menu -->
+				<?php if (get_field('logo', 'option')) : ?>
+					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+						<?php echo wp_get_attachment_image( get_field('logo', 'option'), 'full', "", array( "class" => "img-responsive", "alt" => get_bloginfo( 'name' ) ) ); ?>
+					</a>
+				<?php endif; ?>
+				<!-- end custom logo -->
+			</div>
+			<div id="rinrose_header_nav-right" class="col-5">
+				<span class="navbar-text">
+        			<a href="#">Tours</a>
+      			</span>
+      			<span class="navbar-text">
+        			<a href="#">Apply</a>
+      			</span>
+      		</div>
+      	</div>
 	</div><!-- .container(-fluid) -->
 
 </nav><!-- .site-navigation -->
