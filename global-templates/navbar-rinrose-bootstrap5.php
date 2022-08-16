dbe069d23c9bf7a5bcc71db623eb538a8301febf
tenderling-dev-kayla <110254344+tenderling-dev-kayla@users.noreply.github.com>
2022-08-16 11:27:02 -0500

    add header and footer

--- global-templates/navbar-rinrose-bootstrap5.php
+++ global-templates/navbar-rinrose-bootstrap5.php
@@ -8,43 +8,124 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-
-?>
-<nav id="main-nav" class="navbar sticky-top navbar-light bg-white" aria-labelledby="main-nav-label">
-	<h2 id="main-nav-label" class="screen-reader-text"><?php esc_html_e( 'Main Navigation', 'tenderling' ); ?></h2>
-	<div class="container-fluid">
-		<div class="row w-100 justify-content-between">
-			<div id="rinrose_header_nav-left" class="col-5 justify-content-start d-flex">
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-    				<span class="navbar-toggler-icon"></span>
-    			</button>
-				<span class="navbar-text">
-        			<a class="btn btn-link" href="#">Residents</a>
-      			</span>
-      		</div>
-      		<div id="rinrose_header_nav-mind" class="col-2 justify-content-center d-flex">
-				<!-- Your site title as branding in the menu -->
-				<?php if (get_field('logo', 'option')) : ?>
-					<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
-						<?php echo wp_get_attachment_image( get_field('logo', 'option'), 'full', "", array( "class" => "img-responsive", "alt" => get_bloginfo( 'name' ) ) ); ?>
-					</a>
-				<?php endif; ?>
-				<!-- end custom logo -->
+if(have_rows('header', 'option')) :
+	while(have_rows('header', 'option')): the_row(); ?>
+		<nav id="rinrose_header" class="navbar sticky-top navbar-light bg-white" aria-labelledby="main-nav-label">
+			<h2 id="main-nav-label" class="screen-reader-text"><?php esc_html_e( 'Main Navigation', 'tenderling' ); ?></h2>
+			<div class="container-fluid" id="rinrose_header-collapsed">
+				<div class="row w-100 justify-content-between">
+					<div id="rinrose_header-collapsed_toggle" class="col-5 justify-content-start d-flex">
+						<button id="rinrose_header-collapsed_toggle-btn" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#rinrose_header-expand" aria-controls="rinrose_header-expand" aria-expanded="false" aria-label="Toggle navigation">
+		    				<span class="navbar-toggler-icon"></span>
+		    			</button>
+		    			<?php if (have_rows('left_link')):
+		    				while(have_rows('left_link')): the_row(); ?>
+								<span class="navbar-text" id="rinrose_header-collapsed_toggle-link">
+		        					<a class="btn btn-link" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+		        						<?php the_sub_field('text'); ?>
+		        					</a>
+		      					</span>
+		      				<?php endwhile;
+		      			endif; ?>
+		      		</div>
+		      		<div id="rinrose_header-collapsed_brand" class="col-2 justify-content-center d-flex">
+						<!-- Your site title as branding in the menu -->
+						<?php if (get_sub_field('logo')) : ?>
+							<a id="rinrose_header-collapsed_brand-logo" class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+								<?php echo wp_get_attachment_image( get_sub_field('logo'), 'full', "", array( "class" => "img-fluid", "alt" => get_bloginfo( 'name' ) ) ); ?>
+							</a>
+						<?php endif; ?>
+						<!-- end custom logo -->
+					</div>
+					<div id="rinrose_header-collapsed_ctas" class="col-5 justify-content-end d-flex">
+						<?php if (have_rows('right_link')):
+		    				while(have_rows('right_link')): the_row(); ?>
+								<span class="navbar-text" id="rinrose_header-collapsed_ctas-link">
+		        					<a class="btn btn-link" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+		        						<?php the_sub_field('text'); ?>
+		        					</a>
+		      					</span>
+		      				<?php endwhile;
+		      			endif; ?>
+		      			<?php if (have_rows('right_button')):
+		    				while(have_rows('right_button')): the_row(); ?>
+								<span class="navbar-text" id="rinrose_header-collapsed_ctas-btn">
+		        					<a class="btn btn-outline-primary" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+		        						<?php the_sub_field('text'); ?>
+		        					</a>
+		      					</span>
+		      				<?php endwhile;
+		      			endif; ?>
+		      		</div>
+		      	</div>
+			</div><!-- .container(-fluid) -->
+			<div class="collapse fixed-top w-100 bg-dark" id="rinrose_header-expand">			 	
+		 		<div id="rinrose_header-expand_top" class="row w-100 justify-content-between">
+					<div id="rinrose_header-expand_toggle" class="col-5 justify-content-start d-flex">
+						<button id="rinrose_header-expand_toggle-btn" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#rinrose_header-expand" aria-controls="rinrose_header-expand" aria-expanded="false" aria-label="Toggle navigation">
+		    				<span class="navbar-toggler-icon"></span>
+		    			</button>
+		    			<?php if (have_rows('left_link')):
+		    				while(have_rows('left_link')): the_row(); ?>
+								<span class="navbar-text" id="rinrose_header-expand_toggle-link">
+		        					<a class="btn btn-link" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+		        						<?php the_sub_field('text'); ?>
+		        					</a>
+		      					</span>
+		      				<?php endwhile;
+		      			endif; ?>
+		      		</div>
+		      		<div id="rinrose_header-expand_brand" class="col-2 justify-content-center d-flex">
+						<!-- Your site title as branding in the menu -->
+						<?php if (get_sub_field('logo')) : ?>
+							<a id="rinrose_header-expand_brand-logo" class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url">
+								<?php echo wp_get_attachment_image( get_sub_field('logo'), 'full', "", array( "class" => "img-fluid", "alt" => get_bloginfo( 'name' ) ) ); ?>
+							</a>
+						<?php endif; ?>
+						<!-- end custom logo -->
+					</div>
+					<div id="rinrose_header-expand_ctas" class="col-5 justify-content-end d-flex">
+						<?php if (have_rows('right_link')):
+		    				while(have_rows('right_link')): the_row(); ?>
+								<span class="navbar-text" id="rinrose_header-expand_ctas-link">
+		        					<a class="btn btn-link" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+		        						<?php the_sub_field('text'); ?>
+		        					</a>
+		      					</span>
+		      				<?php endwhile;
+		      			endif; ?>
+		      			<?php if (have_rows('right_button')):
+		    				while(have_rows('right_button')): the_row(); ?>
+								<span class="navbar-text" id="rinrose_header-expand_ctas-btn">
+		        					<a class="btn btn-outline-light" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+		        						<?php the_sub_field('text'); ?>
+		        					</a>
+		      					</span>
+		      				<?php endwhile;
+		      			endif; ?>
+		      		</div>
+	      		</div>
+	      		<div id="rinrose_header-expand_nav">
+	      			<?php $header_menu_args = array(
+					    'menu_class'        => "rinrose_header-menu",
+					    'menu_id'           => "rinrose_header-expand_nav-menu",
+					    'container'         => false,
+					    'theme_location'    => "primary",
+					);
+					wp_nav_menu($header_menu_args); ?>
+				</div>
+				<div id="rinrose_header-expand_footer">
+					<?php if (have_rows('menu_bottom_link')):
+	    				while(have_rows('menu_bottom_link')): the_row(); ?>
+							<span class="navbar-text" id="rinrose_header-expand_footer-link">
+	        					<a class="btn btn-link" href="<?php the_sub_field('link'); ?>" target="<?php the_sub_field('target'); ?>">
+	        						<?php the_sub_field('text'); ?>
+	        					</a>
+	      					</span>
+	      				<?php endwhile;
+	      			endif; ?>
+	      		</div>
 			</div>
-			<div id="rinrose_header_nav-right" class="col-5 justify-content-end d-flex">
-				<span class="navbar-text">
-        			<a class="btn btn-link" href="#">Tours</a>
-      			</span>
-      			<span class="navbar-text">
-        			<a class="btn btn-outline-primary" href="#">Apply</a>
-      			</span>
-      		</div>
-      	</div>
-	</div><!-- .container(-fluid) -->
-	<div class="collapse fixed-top w-100" id="navbarToggleExternalContent">
-	 	<div class="bg-dark p-4">
-	    	<h5 class="text-white h4">Collapsed content</h5>
-	    	<span class="text-muted">Toggleable via the navbar brand.</span>
-		</div>
-	</div>
-</nav><!-- .site-navigation -->
+		</nav><!-- .site-navigation -->
+	<?php endwhile;
+endif;
