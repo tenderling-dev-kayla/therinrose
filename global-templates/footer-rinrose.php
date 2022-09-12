265f57f0f8419f4e75a533183809d54da164855c
tenderling-dev-kayla <110254344+tenderling-dev-kayla@users.noreply.github.com>
2022-09-12 10:26:48 -0500

    ada compliance

--- global-templates/footer-rinrose.php
+++ global-templates/footer-rinrose.php
@@ -26,8 +26,12 @@
 						</div>
 						<?php if(have_rows('social')) : ?>
 							<div id="rinrose_footer-social" class="d-flex">
-								<?php while(have_rows('social')) : the_row(); ?>
-									<a id="rinrose_footer-social_<?php the_sub_field('type'); ?>" class="btn btn-outline-light rounded-circle p-2 me-5" role="button" href="<?php the_sub_field('link'); ?>" target="_blank" aria-label="<?php the_sub_field('type'); ?> profile link - opens in a new tab">
+								<?php while(have_rows('social')) : the_row(); 
+									$field = get_sub_field_object('type');
+									$value = get_sub_field('type');
+									$label = $field['choices'][ $value ];
+									?>
+									<a id="rinrose_footer-social_<?php the_sub_field('type'); ?>" class="btn btn-outline-light rounded-circle p-2 me-5" role="button" href="<?php the_sub_field('link'); ?>" target="_blank" aria-label="<?php echo $label; ?> profile link - opens in a new tab">
 										<i class="fa-brands fa-<?php the_sub_field('type'); ?>" aria-label="<?php the_sub_field('type'); ?>"></i>
 									</a>
 								<?php endwhile; ?>
